333a2caa0c8fae33b9975c8ad121ad488e2d0012
comatoki <hbabnd@gmail.com>
2024-05-06 02:02:01 +0900

    init

---
+++ NotifTest/NotifFeature.swift
@@ -0,0 +1,8 @@
+//
+//  NotifFeature.swift
+//  NotifTest
+//
+//  Created by COMATOKI on 2024-05-05.
+//
+
+import Foundation
---
+++ NotifTest/NotifManager.swift
@@ -0,0 +1,8 @@
+//
+//  NotifManager.swift
+//  NotifTest
+//
+//  Created by COMATOKI on 2024-05-05.
+//
+
+import Foundation
