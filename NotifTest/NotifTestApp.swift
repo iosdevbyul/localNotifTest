66962a28fdd97085d2edda9a06004ca9efca3a1d
comatoki <hbabnd@gmail.com>
2024-05-05 22:53:35 +0900

    Initial Commit

---
+++ NotifTest/NotifTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotifTestApp.swift
+//  NotifTest
+//
+//  Created by COMATOKI on 2024-05-05.
+//
+
+import SwiftUI
+
+@main
+struct NotifTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
